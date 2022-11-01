277b5d2e5edac600d6d0c7b0183da204ee1ebbc2
Emilio Parker <emilio@motmot.tech>
2022-11-01 13:21:19 -0600

    more tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 
+use std::collections::HashMap;
 use std::{sync::Arc};
 
 use hyper::{Request, body, server::conn::AddrStream};
 use serde::{Deserialize, Serialize};
+use tokio::time::error::Elapsed;
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
@@ -28,14 +30,20 @@ struct PlayerResponse {
 
 #[derive(Clone)]
 struct AppContext {
+    data_base : HashMap<String, String>
 }
 
 
 #[tokio::main()]
 async fn main() {
 
+    let mut data_base = HashMap::new();
+    data_base.insert("joe".to_owned(), "<CTO>joe<CTO>".to_owned());
+    data_base.insert("parker".to_owned(), "<Developer><Developer>".to_owned());
     let context = AppContext {
+        data_base
     };
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 3030));
     let make_service = make_service_fn(move |conn: &AddrStream| {
         let context = context.clone();
@@ -69,9 +77,36 @@ async fn handle(context: AppContext, mut req: Request<Body>) -> Result<Response<
     // let data: PlayerRequest = serde_json::from_slice(&data).unwrap();
     println!("got some data {:?}", result);
 
+    let mut user_name : Option<String> = None;
+    for data in result{
+        if data.0.eq("user") {
+            user_name = Some(data.1.to_owned());
+        }
+    }
+
+    if let Some(user_name) = user_name {
+        let assigned_user_name = context.data_base.get(&user_name);
+        if let Some(assigned_user_name) = assigned_user_name {
+
+            let player_response = PlayerResponse {
+                ResultCode :1,
+                UserId : assigned_user_name.to_owned()
+            };
+            let response = serde_json::to_vec(&player_response).unwrap();
+
+            let response = Response::builder()
+            .status(200)
+            .header("Content-Type", "application/json")
+            .body(Body::from(response))
+            .unwrap();
+
+            return Ok(response);
+        }
+    }
+
     let player_response = PlayerResponse {
-        ResultCode :1,
-        UserId : "Parker".to_owned()
+        ResultCode :2,
+        UserId : " ".to_owned()
     };
     let response = serde_json::to_vec(&player_response).unwrap();
 
